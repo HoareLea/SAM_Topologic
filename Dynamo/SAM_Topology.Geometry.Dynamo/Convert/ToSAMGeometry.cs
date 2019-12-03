f3083b2aab4e7d239988dbbf7564554e142ca794
michaldengusiak <tassimulation@gmail.com>
2019-12-03 10:16:34 +0100

    fix structure

---
+++ Dynamo/SAM_Topology.Geometry.Dynamo/Convert/ToSAMGeometry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAM_Topologic.Geometry.Dynamo
+{
+    public static partial class Convert
+    {
+        public static object ToSAMGeometryFromTopologic(Autodesk.DesignScript.Geometry.Geometry geometry)
+        {
+            return 100;
+        }
+    }
+}
