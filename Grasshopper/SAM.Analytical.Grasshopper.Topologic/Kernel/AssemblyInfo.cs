0d78cab9ee0e28ac662eda6a614821b7270de1e2
Michal Dengusiak <tassimulation@gmail.com>
2020-01-22 19:22:58 +0000

    upate resources

---
+++ Grasshopper/SAM.Analytical.Grasshopper.Topologic/Kernel/AssemblyInfo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+
+using Grasshopper.Kernel;
+
+namespace SAM.Analytical.Grasshopper.Topologic
+{
+    public class AssemblyInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "SAM";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return Properties.Resources.SAM_Topologic; ;
+            }
+        }
+
+        public override Bitmap AssemblyIcon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return Properties.Resources.SAM_Topologic; ;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "SAM.Analytical.Grasshopper.Topologic Toolkit, please explore";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("1fdebf9b-0be0-458c-abf8-6436e4b0c4cc");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "Michal Dengusiak & Jakub Ziolkowski at Hoare Lea";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "Michal Dengusiak -> michaldengusiak@hoarelea.com and Jakub Ziolkowski -> jakubziolkowski@hoarelea.com";
+            }
+        }
+    }
+}
