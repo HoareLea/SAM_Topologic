62cfb6b795c3dc7d5bfeeec177d47614b651bce6
Michal Dengusiak <tassimulation@gmail.com>
2020-02-12 16:45:38 +0000

    Update Face.cs

--- SAM_Topologic/SAM.Geometry.Topologic/ToSAM/Face.cs
+++ SAM_Topologic/SAM.Geometry.Topologic/ToSAM/Face.cs
@@ -1,4 +1,5 @@
 ﻿using SAM.Geometry.Spatial;
+using System.Collections.Generic;
 using Topologic;
 
 namespace SAM.Geometry.Topologic
@@ -7,7 +8,16 @@ namespace SAM.Geometry.Topologic
     {
         public static Face3D ToSAM(this global::Topologic.Face face)
         {
-            return new Face3D(ToSAM(face.ExternalBoundary));
+            if (face == null)
+                return null;
+
+            List<Polygon3D> polygon3Ds = new List<Polygon3D>() { ToSAM(face.ExternalBoundary) };
+
+            List<Wire> wires = face.InternalBoundaries;
+            if (wires != null && wires.Count > 0)
+                wires.ForEach(x => polygon3Ds.Add(ToSAM(x)));
+
+            return Face3D.Create(polygon3Ds);
         }
     }
 }
