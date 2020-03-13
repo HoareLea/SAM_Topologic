bf6cd5cd09c412bd54845ab93a49ae2996edba8c
Michal Dengusiak <tassimulation@gmail.com>
2020-03-13 14:42:19 +0000

    Update Point3D due to issue with plane calculation we rounding

--- SAM_Topologic/SAM.Geometry.Topologic/ToSAM/Point3D.cs
+++ SAM_Topologic/SAM.Geometry.Topologic/ToSAM/Point3D.cs
@@ -7,9 +7,12 @@ namespace SAM.Geometry.Topologic
     public static partial class Convert
     {
         //due to issue with plane calculation we rounding 
-        public static Point3D ToSAM(this Vertex vertex, int decimals = Core.Rounding.MicroDistance)
+        public static Point3D ToSAM(this Vertex vertex, int decimals = -1)
         {
-            return new Point3D(Math.Round(vertex.X, decimals), Math.Round(vertex.Y, decimals), Math.Round(vertex.Z, decimals));
+            if (decimals == -1)
+                return new Point3D(vertex.X, vertex.Y, vertex.Z);
+            else
+                return new Point3D(Math.Round(vertex.X, decimals), Math.Round(vertex.Y, decimals), Math.Round(vertex.Z, decimals));
         }
     }
 }
