e93d25eb2355b678155f2f077d891b2aa25007bd
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-02-16 08:32:55 +0000

    SAM.Core.Grasshopper.Topologic Added

---
+++ Grasshopper/SAM.Core.Grasshopper.Topologic/Kernel/AssemblyInfo.cs
@@ -0,0 +1,70 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace SAM.Core.Grasshopper.Topologic
+{
+    public class AssemblyInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "SAM";
+            }
+        }
+
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return Properties.Resources.HL_Logo24; ;
+            }
+        }
+
+        public override Bitmap AssemblyIcon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return Properties.Resources.HL_Logo24; ;
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "SAM Grashopper Toolkit, please explore";
+            }
+        }
+
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("2e814b83-8f0d-4038-8f7a-c0d7832fcdb1");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "Michal Dengusiak & Jakub Ziolkowski at Hoare Lea";
+            }
+        }
+
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "Michal Dengusiak -> michaldengusiak@hoarelea.com and Jakub Ziolkowski -> jakubziolkowski@hoarelea.com";
+            }
+        }
+    }
+}
