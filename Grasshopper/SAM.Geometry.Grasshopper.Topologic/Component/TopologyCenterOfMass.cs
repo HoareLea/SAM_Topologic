409149668ae5857d78a8899629db38c45ab3a2d6
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-04-06 16:43:31 +0100

    CenterOfMass and Centroid Implemented for Topology

---
+++ Grasshopper/SAM.Geometry.Grasshopper.Topologic/Component/TopologyCenterOfMass.cs
@@ -0,0 +1,61 @@
+﻿using Grasshopper.Kernel;
+using SAM.Core.Grasshopper;
+using SAM.Geometry.Grasshopper.Topologic.Properties;
+using System;
+using Topologic;
+
+namespace SAM.Geometry.Grasshopper.Topologic
+{
+    public class TopologyCenterOfMass : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("E8F8CbbA-D3dd-467E-aDEF-e23B86bCFD9e");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Topologic;
+
+        public TopologyCenterOfMass()
+          : base("Topology.CenterOfMass", "Topology.CenterOfMass", "Center Of Mass for Topology", "SAM", "Topologic")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager manager)
+        {
+            manager.AddGenericParameter("_topology", "_topology", "Topology Geometry", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("CenterOfMass", "CenterOfMass", "CenterOfMass", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Topology topology = null;
+
+            if (!DA.GetData(0, ref topology))
+                return;
+
+            DA.SetData(0, topology?.CenterOfMass);
+        }
+    }
+}
---
+++ Grasshopper/SAM.Geometry.Grasshopper.Topologic/Component/TopologyCentroid.cs
@@ -0,0 +1,61 @@
+﻿using Grasshopper.Kernel;
+using SAM.Core.Grasshopper;
+using SAM.Geometry.Grasshopper.Topologic.Properties;
+using System;
+using Topologic;
+
+namespace SAM.Geometry.Grasshopper.Topologic
+{
+    public class TopologyCentroid : GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("70Fb600f-E365-4E40-8033-C8352f6643b4");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Topologic;
+
+        public TopologyCentroid()
+          : base("Topology.Centroid", "Topology.Centroid", "Calculate Centroid of Topology", "SAM", "Topologic")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager manager)
+        {
+            manager.AddGenericParameter("_topology", "_topology", "Topology Geometry", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Centroid", "Centroid", "Centroid", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            Topology topology = null;
+
+            if (!DA.GetData(0, ref topology))
+                return;
+
+            DA.SetData(0, topology?.Centroid);
+        }
+    }
+}
