a9e46162f397c49cd8856d398e67084885be5850
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-03-18 15:45:44 +0100

    CellComplexByCells Grasshopper component implemented

---
+++ Grasshopper/SAM.Analytical.Grasshopper.Topologic/Component/CellComplexByCells.cs
@@ -0,0 +1,88 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using SAM.Analytical.Grasshopper.Topologic.Properties;
+using SAM.Core.Grasshopper;
+using System;
+using System.Collections.Generic;
+using Topologic;
+
+namespace SAM.Analytical.Grasshopper.Topologic
+{
+    public class CellComplexByCells: GH_SAMComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("b59073da-e1e1-4d7e-bc60-fdfe5bf2b924");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Topologic3a;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public CellComplexByCells()
+          : base("CellComplex.ByCells", "CellComplex.ByCells",
+              "Convert Topologic Cells to Topologic CellComplex",
+              "SAM", "Topologic")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager inputParamManager)
+        {
+            inputParamManager.AddGenericParameter("_cells", "_cells", "Topology Cells", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager outputParamManager)
+        {
+            outputParamManager.AddGenericParameter("CellComplex", "CellComplex", "Topology CellComplex", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            List<GH_ObjectWrapper> gH_ObjectWrappers = new List<GH_ObjectWrapper>();
+
+            if (!dataAccess.GetDataList(0, gH_ObjectWrappers) || gH_ObjectWrappers.Count == 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            List<Cell> cells = new List<Cell>();
+            foreach(GH_ObjectWrapper gH_ObjectWrapper in gH_ObjectWrappers)
+            {
+                Cell cell = gH_ObjectWrapper.Value as Cell;
+            }
+
+            
+            if (cells == null || cells.Count == 0)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            CellComplex cellComplex = CellComplex.ByCells(cells);
+
+            dataAccess.SetData(0, cellComplex);
+        }
+    }
+}
