403c8c8c2cb3337967d1b4d2a40abc895ca36dce
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-02-20 11:54:54 +0100

    ActiveSetting loading bug fixed

--- SAM_Topologic/SAM.Core.Topologic/Manager/ActiveSettings.cs
+++ SAM_Topologic/SAM.Core.Topologic/Manager/ActiveSettings.cs
@@ -9,13 +9,15 @@ namespace SAM.Core.Revit
             public const string ParameterName_Topology = "ParameterName_Topology";
         }
 
-        private static Setting setting = Load();
+        private static Setting setting = null;
 
         private static Setting Load()
         {
             Setting setting = ActiveManager.GetSetting(Assembly.GetExecutingAssembly());
             if (setting == null)
+            {
                 setting = GetDefault();
+            }
 
             return setting;
         }
@@ -24,6 +26,11 @@ namespace SAM.Core.Revit
         {
             get
             {
+                if(setting == null)
+                {
+                    setting = Load();
+                }
+
                 return setting;
             }
         }
